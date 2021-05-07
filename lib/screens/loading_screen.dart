73eaac65474e931234f135a71efedf608ee36df8
vaibhav6900 <starryvaibh786@gmail.com>
2021-05-07 13:52:02 +0530

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,50 @@
+import 'package:clima/services/networking.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/services/weather.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:core';
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void initState(){
+    super.initState();
+    getLocationData();
+
+  }
+  void getLocationData () async
+  {
+    WeatherModel weatherModel=WeatherModel();
+    var weatherData= await weatherModel.getLocationWeather();
+
+   Navigator.push(context, MaterialPageRoute(builder: (context)
+    {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+
+    }));
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Center(
+       child: SpinKitThreeBounce(
+         color: Colors.white,
+         size: 100.0,
+       ),
+     ),
+    );
+  }
+}
